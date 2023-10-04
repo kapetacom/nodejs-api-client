4a33be3ab6391edd594728b6b1e113bf4771172f
Henrik Hofmeister <henrik@newdawn.dk>
2023-10-04 13:17:37 +0200

    Fix text

--- src/KapetaAPI.ts
+++ src/KapetaAPI.ts
@@ -133,7 +133,7 @@ export class KapetaAPI {
         try {
             return JSON.parse(Buffer.from(process.env.KAPETA_CREDENTIALS_TOKEN, 'base64').toString('ascii')).token;
         } catch (e) {
-            console.log(`Failed to parse JWT token. Got ${e}`);
+            console.log(`Failed to parse JWT token from KAPETA_CREDENTIALS_TOKEN. Got ${e}`);
             return null;
         }
     }
